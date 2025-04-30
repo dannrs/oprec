37e775fa482d2b9aac6efef4bb4a47f273d23727
dannrs <danniramdhani@proton.me>
2025-04-30 23:18:11 +0700

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'es5',
+    printWidth: 80,
+    tabWidth: 2,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    plugins: ['prettier-plugin-tailwindcss'],
+  };
+
+export default config;
