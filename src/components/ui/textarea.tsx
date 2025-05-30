ecb6e108caaf877df08f6e7731775b425c43c5c8
dannrs <danniramdhani@proton.me>
2025-05-30 10:22:45 +0700

    small ui adjustments

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -8,7 +8,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          'border-input bg-background flex min-h-[80px] w-full rounded-md border px-3 py-2 text-sm shadow-sm',
+          'border-input dark:bg-input/30 flex min-h-[80px] w-full rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm',
           'placeholder:text-muted-foreground focus-visible:ring-ring focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none',
           'disabled:cursor-not-allowed disabled:opacity-50',
           className
